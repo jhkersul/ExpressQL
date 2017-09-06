ba154e8c397ac0e9aba54a8abbd820ed7a07bcf9
Kersul <joaohkfaria@gmail.com>
2017-09-06 11:02:12 -0300

    Refactoring DateFormatter as a service on a functional approach

---
+++ services/DateFormatter.js
@@ -0,0 +1,35 @@
+/**
+ * Gets the current date in ISO format
+ * @return {String} The current date string in ISO format
+ */
+export const currentDateISOString = () => {
+  const date = new Date();
+
+  return date.toISOString();
+};
+
+/**
+ * Converts a Date to ISO string date
+ * @param  {Date} date The date that we wanna convert
+ * @return {String}    A date string in ISO format
+ */
+export const dateToISOString = (date) => {
+  if (date instanceof Date) return date.toISOString();
+
+  return null;
+};
+
+/**
+ * Converts a ISO string date to Date
+ * @param  {[type]} isoString A date string in ISO format that we wanna convert
+ * @return {[type]}           A date object
+ */
+export const isoStringToDate = (isoString) => {
+  if (typeof isoString === 'undefined' || isoString === null) {
+    return null;
+  }
+
+  const date = new Date(isoString);
+
+  return date;
+};
