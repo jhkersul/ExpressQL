131649256456a67fe6ba56904797dbf0cd4a2897
Kersul <joaohkfaria@gmail.com>
2017-09-06 12:16:41 -0300

    Adding comments to watchmen restart script

--- restartWatchman.sh
+++ restartWatchman.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+
+### This is a script to restart watchman
+### Sometimes, watchman has some bugs
+### And we need this script to restart and get things back to work
+
 echo 256 | sudo tee -a /proc/sys/fs/inotify/max_user_instances
 echo 32768 | sudo tee -a /proc/sys/fs/inotify/max_queued_events
 echo 65536 | sudo tee -a /proc/sys/fs/inotify/max_user_watches
