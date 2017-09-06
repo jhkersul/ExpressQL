a47e54981ffa1af0c63aca3736cdbd3514ab3c2b
Kersul <joaohkfaria@gmail.com>
2017-09-06 11:40:32 -0300

    Changing DateFormatter functions names

--- services/DateFormatter.js
+++ services/DateFormatter.js
@@ -2,7 +2,7 @@
  * Gets the current date in ISO format
  * @return {String} The current date string in ISO format
  */
-export const currentDateISOString = () => {
+export const getCurrentISOStringDate = () => {
   const date = new Date();
 
   return date.toISOString();
@@ -13,7 +13,7 @@ export const currentDateISOString = () => {
  * @param  {Date} date The date that we wanna convert
  * @return {String}    A date string in ISO format
  */
-export const dateToISOString = (date) => {
+export const getISOStringFromDate = (date) => {
   if (date instanceof Date) return date.toISOString();
 
   return null;
@@ -24,7 +24,7 @@ export const dateToISOString = (date) => {
  * @param  {[type]} isoString A date string in ISO format that we wanna convert
  * @return {[type]}           A date object
  */
-export const isoStringToDate = (isoString) => {
+export const getDateFromISOString = (isoString) => {
   if (typeof isoString === 'undefined' || isoString === null) {
     return null;
   }
